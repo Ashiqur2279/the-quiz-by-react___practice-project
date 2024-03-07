f368816f889deda84c11a0466ab64b13730bf590
Ashiqur2279 <ashiqurrahman2279@gmail.com>
2024-03-07 10:34:46 +0600

    data is loaded

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,76 @@
 // import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  //'loading','error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("action is unknown");
+  }
+};
+
 function App() {
-  const [data, setData] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //destructure the state
+  const { questions } = state;
+
+  //option 1: fetch the data using the json server
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/questions")
+  //     .then((res) => {
+  //       if (!res.ok) throw new Error("Network response is not ok");
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("there is the error to fetch the data", error);
+  //     });
+  // }, []);
+
+  // option 2: fetch the data using local file
   useEffect(() => {
-    fetch("../assets/data/questions.json")
+    fetch("src/assets/data/questions.json")
       .then((res) => {
-        if (!res.ok) throw new Error("Network response is not ok");
+        if (!res.ok) throw new Error("network is not responding");
         return res.json();
       })
       .then((data) => {
-        console.log(data);
-        setData(data);
+        if (data) dispatch({ type: "dataReceived", payload: data.questions });
       })
-      .catch((error) => {
-        console.error("there is the error to fetch the data", error);
-      });
+      .catch(() => dispatch({ type: "dataFailed" }));
   }, []);
-  console.log(data.questions);
+
   return (
     <div className="app">
       <Header />
       <Main>
+        {/* {questions.map((question) => (
+          <h2 key={question}>{question}</h2>
+        ))} */}
         <p>1/15</p>
         <p>Questions</p>
       </Main>
